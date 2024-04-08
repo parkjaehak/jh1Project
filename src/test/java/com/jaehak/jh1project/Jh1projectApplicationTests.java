0972106945231e9de6ed61248e06afc15a1093a2
parkjaehak <atom8426@naver.com>
2024-04-08 18:20:18 +0900

    1test

---
+++ src/test/java/com/jaehak/jh1project/Jh1projectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaehak.jh1project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jh1projectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
